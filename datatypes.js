325d8191fa523504b5a376a73f7c37d8fe84df6a
Sibongile <sibongilearathokwane@gmail.com>
2025-07-11 09:21:55 +0200

    added data types

--- datatypes.js
+++ datatypes.js
@@ -1,28 +1,32 @@
 //String example//
-let petName = 'Teddy' // Global variable
+let petName = "Teddy"; // Global variable
 
-myFunction()
+myFunction();
 function myFunction() {
-fruit = 'apple'; // Considered global
-console.log(typeof petName + '' + 'My pet name is ' +petName)
+  fruit = "apple"; // Considered global
+  console.log(typeof petName + "" + "My pet name is " + petName);
 }
-console.log(typeof petName + '-' + 'My pet name is ' + petName + 'Fruit name is ' + fruit)
+console.log(
+  typeof petName + "-" + "My pet name is " + petName + "Fruit name is " + fruit
+);
 
 ///////
 
 //////INTEGER EXAMPLE///////
 // Integer example
-let numberOfApples = 10;           // Integer value
-fruits()
-function fruits(){
-console.log(typeof numberOfApples + '- ' + 'Number of apples is: ' + numberOfApples);          
+let numberOfApples = 10; // Integer value
+fruits();
+function fruits() {
+  console.log(
+    typeof numberOfApples + "- " + "Number of apples is: " + numberOfApples
+  );
 }
 
 /////FLOAT/DOUBLE EXAMPLE///
 let price = 10.99;
-money()
-function money(){
-console.log(typeof price + '-' + 'total money is: ' + price);   
+money();
+function money() {
+  console.log(typeof price + "-" + "total money is: " + price);
 }
 
 /////BOOLEAN EXAMPLE///////////
@@ -33,18 +37,31 @@ let isExpired = false;
 coffee();
 
 function coffee() {
-  console.log(typeof isExpired + ' - coffee is expired: ' + isExpired);
-  console.log(typeof isAvailable + ' - coffee is available: ' + isAvailable);
+  console.log(typeof isExpired + " - coffee is expired: " + isExpired);
+  console.log(typeof isAvailable + " - coffee is available: " + isAvailable);
 }
 
 ///////character datat type example///
-let studentInitial = 'S';  // Character (string of length 1)
+let studentInitial = "S"; // Character (string of length 1)
 
 show(); // Call the function
 
 function show() {
-  console.log(typeof studentInitial + ' - student initial: ' + studentInitial);
+  console.log(typeof studentInitial + " - student initial: " + studentInitial);
 }
 
+/////symbol data type example////
+let id = Symbol("id");
+showsymbolType();
 
+function showsymbolType() {
+  console.log(typeof id + "- $ ");
+}
 
+/// Show object example///
+let colours = ["red,blue, yellow"];
+nothing();
+
+function nothing() {
+  console.log(typeof colours + "-red,blue, yellow");
+}
